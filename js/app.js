3f928fa79c08d62cb2429859597e6ea0f01a391f
zickbalazs <zickbalazs@turr.hu>
2022-04-12 12:24:52 +0200

    Kábeltévé elkezdve

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let navBtn = document.querySelector('button'),
     linkek = document.querySelector('nav').querySelector('div'),
     _switch = false;
+    arrows = document.querySelectorAll('i');
 navBtn.addEventListener('click', ()=>{
     if (_switch){
         linkek.style.height = "0";
@@ -12,6 +13,26 @@ navBtn.addEventListener('click', ()=>{
     }
     _switch=!_switch
 });
+
+for (let i = 1; i < arrows.length; i++) {
+    arrows[i].addEventListener('click', ()=>{
+        let div = document.querySelector(`.a${i}`);
+        if (div.style.display==""){
+            div.style.display="none";
+        }
+        if (div.style.display=="none"){
+            div.style.display="block";
+            div.style.opacity = "100%";
+            arrows[i].style.transform="rotate(180deg)"
+        }
+        else if (div.style.display=="block"){
+            div.style.display="none";
+            div.style.opacity = "0%";
+            arrows[i].style.transform="rotate(0deg)"
+        }
+    });
+}
+
 
 window.addEventListener('resize', ()=>{
     if (window.screen.width>679){
